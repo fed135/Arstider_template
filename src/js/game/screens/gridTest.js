9d39fa21d953972e16f708360ce2deb2108b6631
Fred C <fredericcharette@gmail.com>
2014-03-20 15:15:35 -0400

    gridMap tests

---
+++ src/js/game/screens/gridTest.js
@@ -0,0 +1,26 @@
+define("screens/gridTest", [
+		"Arstider/Screen", 
+		"Arstider/DisplayObject", 
+		"Arstider/GridMap",
+		
+		"textLib!../media/maps/map01.json"
+	], function(Screen, DisplayObject, GridMap, map){
+		
+		var level = new GridMap(JSON.parse(map));
+		
+		
+		function GridTest(name){
+			Arstider.Super(this, Screen, name);
+			
+			this.addChild(level);
+		}
+		
+		Arstider.Inherit(GridTest, Screen);
+		
+		GridTest.prototype.onload = function(){
+			console.log(level);
+		};
+		
+		return GridTest;
+	}
+);
